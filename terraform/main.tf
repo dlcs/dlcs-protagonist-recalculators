667d35c15beb7958c48630a8cefbd658cc4cfbf7
Donald Gray <donald.gray@digirati.com>
2023-10-10 18:03:49 +0100

    Add type + description to variables
    Also ran terraform fmt and remove unused variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,19 @@
-resource  "aws_cloudwatch_log_group" "lambda_docker_log_group" {
-  name = "/aws/lambda/${var.function_name}"
+resource "aws_cloudwatch_log_group" "lambda_docker_log_group" {
+  name              = "/aws/lambda/${var.function_name}"
   retention_in_days = var.retention_in_days
 }
 
 resource "aws_lambda_function" "lambda_docker_function" {
   function_name = var.function_name
-  package_type = "Image"
+  package_type  = "Image"
   role          = aws_iam_role.lambda_docker_function_exec_role.arn
   image_uri     = var.image_uri
   timeout       = var.lambda_timeout
 
   vpc_config {
-      subnet_ids         = var.subnet_ids
-      security_group_ids = var.security_group_ids
-    }
+    subnet_ids         = var.subnet_ids
+    security_group_ids = var.security_group_ids
+  }
 
   environment {
     variables = var.environment
@@ -39,8 +39,8 @@ resource "aws_iam_role" "lambda_docker_function_exec_role" {
 }
 
 resource "aws_iam_role_policy" "lambda_docker_function_logging" {
-  name               = "${var.function_name}-exec-role"
-  role       = aws_iam_role.lambda_docker_function_exec_role.name
+  name   = "${var.function_name}-exec-role"
+  role   = aws_iam_role.lambda_docker_function_exec_role.name
   policy = data.aws_iam_policy_document.lambda_docker_function_permissions.json
 }
 
@@ -64,20 +64,20 @@ data "aws_iam_policy_document" "lambda_docker_function_permissions" {
 }
 
 resource "aws_cloudwatch_event_rule" "lambda_docker_cloudwatch_cron_rule" {
-  name = "${var.function_name}-cloudwatch-cron-rule"
-  description = "${var.function_name} CRON rule for scheduling runs"
+  name                = "${var.function_name}-cloudwatch-cron-rule"
+  description         = "${var.function_name} CRON rule for scheduling runs"
   schedule_expression = "cron(${var.cron_schedule})"
 }
 
 resource "aws_lambda_permission" "lambda_docker_cron_permission" {
-  statement_id = "${var.function_name}-allow-cloudwatch-execution"
-  action = "lambda:InvokeFunction"
+  statement_id  = "${var.function_name}-allow-cloudwatch-execution"
+  action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda_docker_function.arn
-  principal = "events.amazonaws.com"
-  source_arn = aws_cloudwatch_event_rule.lambda_docker_cloudwatch_cron_rule.arn
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.lambda_docker_cloudwatch_cron_rule.arn
 }
 
 resource "aws_cloudwatch_event_target" "lambda_docker_cloudwatch_target" {
   rule = aws_cloudwatch_event_rule.lambda_docker_cloudwatch_cron_rule.name
-  arn = aws_lambda_function.lambda_docker_function.arn
+  arn  = aws_lambda_function.lambda_docker_function.arn
 }
