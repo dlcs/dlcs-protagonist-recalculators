38474729803a20af826877faa9a3e9f7766d279f
Donald Gray <donald.gray@digirati.com>
2023-10-18 12:12:09 +0100

    Add terraform outputs

---
+++ terraform/output.tf
@@ -0,0 +1,11 @@
+output "function_name" {
+  value = aws_lambda_function.lambda_docker_function.name
+}
+
+output "function_arn" {
+  value = aws_lambda_function.lambda_docker_function.arn
+}
+
+output "function_role_arn" {
+  value = aws_iam_role.lambda_docker_function_exec_role.arn
+}
